d8ffaf9f47ace2e2cb11a0eb86d3f9bd1089ca4b
galactrax <anthonycweathersby@gmail.com>
2020-03-05 08:14:34 -0700

    Convert spark.mjs to spark.ts

--- source/spark.ts
+++ source/spark.ts
@@ -3,12 +3,34 @@
  *
  * Depending on the platform, caller will either map to requestAnimationFrame or it will be a setTimout.
  */
- 
-const caller = (typeof(window) == "object" && window.requestAnimationFrame) ? window.requestAnimationFrame : (f) => {
+const caller = (typeof (window) == "object" && window.requestAnimationFrame) ? window.requestAnimationFrame : (f) => {
     setTimeout(f, 1);
 };
 
-const perf = (typeof(performance) == "undefined") ? { now: () => Date.now() } : performance;
+const perf = (typeof (performance) == "undefined") ? { now: () => Date.now() } : performance;
+/**
+ * Interface used by objects that are have an
+ * update scheduled by Spark
+ */
+interface Sparky {
+
+    /**
+     * Called when the scheduling requirements of the
+     * the Sparky object is met.
+     * @param step_ratio 
+     * @param diff 
+     */
+    scheduledUpdate(step_ratio: number, diff: number);
+    /**
+     * Used internally by Spark. Stores scheduling information.
+     * 
+     * Spark will create this property on a Sparky object if it is 
+     * not already present.
+     * 
+     * @private
+     */
+    _SCHD_: number;
+}
 
 
 /**
@@ -17,43 +39,46 @@ const perf = (typeof(performance) == "undefined") ? { now: () => Date.now() } : 
  * If any object is scheduled to be updated, it will be blocked from scheduling more updates until the next ES VM tick.
  */
 class Spark {
-    /**
-     * Constructs the object.
-     */
+    update_queue_a: Sparky[];
+    update_queue_b: Sparky[];
+    update_queue: Sparky[];;
+    queue_switch: number;
+    frame_time: number;
+    SCHEDULE_PENDING: boolean;
+    callback: () => void;
+
     constructor() {
 
         this.update_queue_a = [];
         this.update_queue_b = [];
-
         this.update_queue = this.update_queue_a;
-
         this.queue_switch = 0;
+        this.callback = () => { };
 
-        this.callback = ()=>{}
-
-
-        if(typeof(window) !== "undefined"){
-            window.addEventListener("load",()=>{
+        if (typeof (window) !== "undefined") {
+            window.addEventListener("load", () => {
                 this.callback = () => this.update();
                 caller(this.callback);
-            })
-        }else{
+            });
+        } else {
             this.callback = () => this.update();
         }
 
-
         this.frame_time = perf.now();
-
         this.SCHEDULE_PENDING = false;
     }
 
     /**
-     * Given an object that has a _SCHD_ Boolean property, the Scheduler will queue the object and call its .update function 
+     * Given an object that has a _SCHD_ Boolean property, the Scheduler will queue the object and call its .scheduledUpdate function
      * the following tick. If the object does not have a _SCHD_ property, the Scheduler will persuade the object to have such a property.
-     * 
-     * If there are currently no queued objects when this is called, then the Scheduler will user caller to schedule an update.
      */
-    queueUpdate(object, timestart = 1, timeend = 0) {
+    /**
+     * 
+     * @param object 
+     * @param time_start 
+     * @param time_end - Tha m 
+     */
+    queueUpdate(object: Sparky, time_start: number = 1, time_end: number = 0) {
 
         if (object._SCHD_ || object._SCHD_ > 0) {
             if (this.SCHEDULE_PENDING)
@@ -62,31 +87,27 @@ class Spark {
                 return caller(this.callback);
         }
 
-        object._SCHD_ = ((timestart & 0xFFFF) | ((timeend) << 16));
+        object._SCHD_ = ((time_start & 0xFFFF) | ((time_end) << 16));
 
         this.update_queue.push(object);
 
-        if (this._SCHD_)
-            return;
-
         this.frame_time = perf.now() | 0;
 
-
-        if(!this.SCHEDULE_PENDING){
+        if (!this.SCHEDULE_PENDING) {
             this.SCHEDULE_PENDING = true;
             caller(this.callback);
         }
     }
 
-    removeFromQueue(object){
+    removeFromQueue(object) {
 
-        if(object._SCHD_)
-            for(let i = 0, l = this.update_queue.length; i < l; i++)
-                if(this.update_queue[i] === object){
-                    this.update_queue.splice(i,1);
+        if (object._SCHD_)
+            for (let i = 0, l = this.update_queue.length; i < l; i++)
+                if (this.update_queue[i] === object) {
+                    this.update_queue.splice(i, 1);
                     object._SCHD_ = 0;
 
-                    if(l == 1)
+                    if (l == 1)
                         this.SCHEDULE_PENDING = false;
 
                     return;
@@ -100,13 +121,14 @@ class Spark {
 
         this.SCHEDULE_PENDING = false;
 
-        const uq = this.update_queue;
-        const time = perf.now() | 0;
-        const diff = Math.ceil(time - this.frame_time) | 1;
-        const step_ratio = (diff * 0.06); //  step_ratio of 1 = 16.66666666 or 1000 / 60 for 60 FPS
+        const
+            uq = this.update_queue,
+            time = perf.now() | 0,
+            diff = Math.ceil(time - this.frame_time) | 1,
+            step_ratio = (diff * 0.06); //  step_ratio of 1 = 16.66666666 or 1000 / 60 for 60 FPS
 
         this.frame_time = time;
-        
+
         if (this.queue_switch == 0)
             (this.update_queue = this.update_queue_b, this.queue_switch = 1);
         else
@@ -117,7 +139,7 @@ class Spark {
             let timeend = ((o._SCHD_ >> 16) & 0xFFFF);
 
             o._SCHD_ = 0;
-            
+
             if (timestart > 0) {
                 this.queueUpdate(o, timestart, timeend);
                 continue;
@@ -125,27 +147,37 @@ class Spark {
 
             timestart = 0;
 
-            if (timeend > 0) 
+            if (timeend > 0)
                 this.queueUpdate(o, timestart, timeend - diff);
 
             /** 
-                To ensure on code path doesn't block any others, 
+                To ensure one code path doesn't block any others with an exception, 
                 scheduledUpdate methods are called within a try catch block. 
                 Errors by default are printed to console. 
             **/
             try {
                 o.scheduledUpdate(step_ratio, diff);
             } catch (e) {
-                console.error(e);
+                this.handleError(e);
             }
         }
 
         uq.length = 0;
     }
+    /**
+     * Override this method to handle errors 
+     * encountered when calling Sparky.queueUpdate.
+     * 
+     * Default action logs the error to console.
+     * 
+     * @param e - error object
+     */
+    handleError(e: any) {
+        console.log(e);
+    }
 }
 
 const spark = new Spark();
 
-export { Spark as SparkConstructor };
-
+export { Spark as SparkConstructor, Sparky };
 export default spark;
